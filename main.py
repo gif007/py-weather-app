0fa91484d7f5ab3a3cad10b9ee9f404fc24ddeb4
bard <carruthers.n.t@gmail.com>
2021-05-17 18:35:10 -0300

    added a top level menubar with a Views dropdown that currently contains a reference to the 24 hour forecast

--- main.py
+++ main.py
@@ -2,9 +2,24 @@
 import frames
 from tkinter import *
 
+currentView = None
+
+def viewTwentyFour(parent):
+    global currentView
+    if not currentView == 'twentyfour':
+        TwentyFourHourForecast = frames.Forecast(parent)
+        currentView = 'twentyfour'
+        return TwentyFourHourForecast
+    else:
+        return
+
 
 if __name__ == '__main__':
     root = Tk()
     root.title('Weather Forecast')
-    TwentyFourHourForecast = frames.Forecast(root)
+    menubar = Menu(root)
+    root['menu'] = menubar
+    menu_views = Menu(menubar)
+    menubar.add_cascade(menu=menu_views, label='Views')
+    menu_views.add_command(label='24 Hour Forecast', command=(lambda: viewTwentyFour(root)))
     root.mainloop()
